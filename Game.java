22cf8e2b355b797ad9fd8bb2da20ead4ab8ad5a2
rodriguezd14 <114750206+rodriguezd14@users.noreply.github.com>
2023-04-09 21:47:04 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -15,10 +15,59 @@ public class Game
 		
 		char c = input.next().charAt(0);
 		
+		if(c == 'c')
+		{
+			System.out.println("For cities in Massachusetts try using captial letters.");
+		}
+		
 		HangmanGame hGame = new HangmanGame(c);
+		
+		//For Debugging
 		System.out.println(hGame.getWordToSolve());
+		
+		//Prepare the game by setting every letter in the word to " __ "
 		String[] arr = hGame.startingWord();
+		
+		//Prints the starting game
 		printWord(arr);
+		
+		while(hGame.getGameStatus() < 5)
+		{
+			//Prompts the user to pick a letter
+			System.out.println();
+			System.out.println("Pick a letter: ");
+			
+			//Store the letter chosen
+			String letter = input.next();
+			
+			if(hGame.letterAlreadyTaken(letter)) 
+			{
+				System.out.println(letter + " is already taken");
+				System.out.println("Your game status is: " + hGame.getGameStatus());
+			}
+			
+			//Checks if the letter exist and stores it in a boolean
+			else if(hGame.checkLetter(letter))
+			{
+				//Updates the array if the letter exists
+				hGame.updateCurWord(hGame.checkLetter(letter));
+				
+				//print the word thats been updated
+				printWord(hGame.getCurword());
+				
+				if(hGame.isGameWon())
+				{
+					System.out.println("congrats");
+					System.exit(0);
+				}
+			}
+			else if(!hGame.checkLetter(letter))
+			{
+				//Get current game status
+				System.out.println("Your game status is: " + hGame.getGameStatus());
+			}
+		}
+		System.out.println("So sorry but you lost.");
 	}
 	
 	public static void printWord(String[] word)
@@ -27,5 +76,6 @@ public class Game
 		{
 			System.out.print(word[i]);
 		}
+		System.out.println();
 	}
 }
