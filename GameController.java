ba89c7af943f7954930f14dc80e29edd26e064d8
rodriguezd14 <114750206+rodriguezd14@users.noreply.github.com>
2023-04-13 01:04:50 -0400

    Create GameController.java

---
+++ GameController.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GameController {
+
+    @FXML
+    private Text displayText;
+
+    @FXML
+    private Pane gameBoard;
+    
+}
